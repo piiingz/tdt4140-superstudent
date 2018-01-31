69c75221f558b9b698e1fd28905501b2693fd95d
Alexander Michael Staff <alexamst@stud.ntnu.no>
2018-01-31 14:32:27 +0100

    Update FxAppTest.java

--- tdt4140-gr1824/app.ui/src/test/java/tdt4140/gr1824/app/ui/FxAppTest.java
+++ tdt4140-gr1824/app.ui/src/test/java/tdt4140/gr1824/app/ui/FxAppTest.java
@@ -12,10 +12,10 @@ public class FxAppTest extends ApplicationTest {
 
 	@Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FxApp.fxml"));
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+    //    Parent root = FXMLLoader.load(getClass().getResource("FxApp.fxml"));
+    //    Scene scene = new Scene(root);
+    //    stage.setScene(scene);
+    //    stage.show();
     }
 
     @Test
