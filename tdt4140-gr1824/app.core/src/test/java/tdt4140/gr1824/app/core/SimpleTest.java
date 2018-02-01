ecf5416a5cb9b45238454da10d29c79f7bdcb2b9
Robin <Robin@192.168.0.237>
2018-02-01 22:19:53 +0100

    #2 Created SimpleTest which passes.

---
+++ tdt4140-gr1824/app.core/src/test/java/tdt4140/gr1824/app/core/SimpleTest.java
@@ -0,0 +1,14 @@
+package tdt4140.gr1824.app.core;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SimpleTest {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
