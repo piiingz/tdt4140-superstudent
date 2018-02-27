9c2a971198f80cbc71b8fca871496b8212923d09
Alexander <Alexander@10.22.67.25>
2018-02-27 11:33:53 +0100

    #10 Configured JaCoCo to find test coverage.
    Also commented out Assert import in AreaTest class.

--- tdt4140-gr1824/app.core/src/test/java/tdt4140/gr1824/app/core/AreaTest.java
+++ tdt4140-gr1824/app.core/src/test/java/tdt4140/gr1824/app/core/AreaTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import org.junit.Assert;
+//import org.junit.Assert;
 
 public class AreaTest {
 
