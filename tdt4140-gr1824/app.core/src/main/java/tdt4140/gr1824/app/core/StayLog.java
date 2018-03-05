53f2a28102ce5d1d945a52b000634c665b0e416c
MariaHilmoJensen <MariaHilmoJensen@dhcp-10-22-75-16.wlan.ntnu.no>
2018-03-05 19:12:11 +0100

    #6 Written StayLogTest.
    Pair programmed with Adam

--- tdt4140-gr1824/app.core/src/main/java/tdt4140/gr1824/app/core/StayLog.java
+++ tdt4140-gr1824/app.core/src/main/java/tdt4140/gr1824/app/core/StayLog.java
@@ -52,5 +52,9 @@ public class StayLog {
 		return stayTime;
 	}	
 
+	public long getStartTime() {
+		return inTime;
+	}
+
 	
 }
--- tdt4140-gr1824/app.core/src/test/java/tdt4140/gr1824/app/core/StayLogTest.java
+++ tdt4140-gr1824/app.core/src/test/java/tdt4140/gr1824/app/core/StayLogTest.java
@@ -1,24 +1,64 @@
 package tdt4140.gr1824.app.core;
 
+import static java.util.concurrent.TimeUnit.NANOSECONDS;
 import static org.junit.Assert.assertTrue;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.junit.Assert;
 import org.junit.Test;
 
 public class StayLogTest {
-	
-	StayLog sl = new StayLog(DefinedAreas.glos, 1);
-	StayLog sl1 = new StayLog(DefinedAreas.sitTrening, 2);
+		
+	@Test
+	public void testStartTime() {
+		StayLog sl = new StayLog(DefinedAreas.sitTrening, 2);
+		long testTime = System.nanoTime();
+		
+		Assert.assertEquals(testTime, sl.getStartTime(), 1);
+		
+	}	
 	
 	@Test
-	public void testsetStayTime() {
+	public void testStayTime() {
+		StayLog sl = new StayLog(DefinedAreas.glos, 1);
+		
 		try {
 			Thread.sleep(1000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 		
-		assertTrue(sl.getStayTime() == 0);
+		sl.stopStayLog();
+		Assert.assertEquals(sl.getStayTime(), (NANOSECONDS.toSeconds(System.nanoTime() - sl.getStartTime()))/60);
 	}
+	
+	@Test
+	public void testFileWriting() {
+		StayLog sl = new StayLog(DefinedAreas.glos, 1);
+		sl.stopStayLog();
+		
+		String lastLine = null;
+		
+		try {
+			BufferedReader br = new BufferedReader(new FileReader("database.txt"));
+			
+			while (br.ready()) {
+				lastLine = br.readLine();
+			}
+			
+			br.close();
+			
+		} catch (IOException e) {
+			System.err.println("Could not open file.");
+		}
+		
+		assertTrue(lastLine.equals("1, glos, 0"));
+		
+	}
+	
 	
 
 }
