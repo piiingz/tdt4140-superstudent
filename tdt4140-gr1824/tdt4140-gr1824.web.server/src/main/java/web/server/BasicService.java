c9bff22a570a6f1c78ff070e7c04f1be08259b89
Alexander <Alexander@LAPTOP-DK5TCNIN.lan>
2018-04-09 12:15:53 +0200

    #11 BasicService
    Pusher for å kunne jobbe parallelt.

---
+++ tdt4140-gr1824/tdt4140-gr1824.web.server/src/main/java/web/server/BasicService.java
@@ -0,0 +1,22 @@
+package web.server;
+
+import java.sql.*;
+
+public class BasicService {
+	
+	Connection con = null;
+	
+	public BasicService() {
+		String url = "jdbc:mysql://mysql.stud.ntnu.no:3306/ingviun_superstudent?useSSL=false";
+		String username = "espesorh_db";
+		String password = "DB168";
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			con = DriverManager.getConnection(url, username, password);
+						
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
