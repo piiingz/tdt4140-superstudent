c8f73331e48ddc1c4fda91343d06f89ef3133b13
piiingz <ingviun@stud.ntnu.no>
2018-04-15 16:01:58 +0200

    Removed main method from dbCom

--- tdt4140-gr1824/app.core/src/main/java/tdt4140/gr1824/app/db/DatabaseCommunicator.java
+++ tdt4140-gr1824/app.core/src/main/java/tdt4140/gr1824/app/db/DatabaseCommunicator.java
@@ -672,21 +672,7 @@ public class DatabaseCommunicator {
 		
 		return goal;
 	}
-	/*
-	private static LocalDateTime getLocalDateTime (String stopTime) {
-		
-		
-		
-		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-		LocalDateTime now = LocalDateTime.now();
-		//System.out.println(dtf.format(now)); //2016/11/16 12:08:43
-		return ""+String.format("%1$tY-%1$tm-%1$td", now);
-		
-	}
 
-	public static void main(String[] args) {
-		System.out.println(getTodaysDate());
-	}*/
 }
 
 
