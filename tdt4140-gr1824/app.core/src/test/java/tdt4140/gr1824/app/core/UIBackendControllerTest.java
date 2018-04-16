05787a889c7957e166b21d0dc2b05d5ad2da7839
Adam <Adam@Adam-PC>
2018-04-16 12:23:38 +0200

    #49 Added some UIBackend tests

--- tdt4140-gr1824/app.core/src/test/java/tdt4140/gr1824/app/core/UIBackendControllerTest.java
+++ tdt4140-gr1824/app.core/src/test/java/tdt4140/gr1824/app/core/UIBackendControllerTest.java
@@ -7,6 +7,10 @@ import java.time.LocalDate;
 
 import org.junit.Test;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import tdt4140.gr1824.app.db.DatabaseCommunicator;
+
 
 public class UIBackendControllerTest {
 	UIBackendController backendController = new UIBackendController();
@@ -28,5 +32,29 @@ public class UIBackendControllerTest {
 		assertTrue(this.backendController.getLinePointsUser(1,LocalDate.parse("2018-04-02"), LocalDate.parse("2018-06-02"), "glos").length <= 8);
 	}
 
+	@Test
+	public void testCompetitions() throws SQLException {
+		ObservableList<String> allCompetitionNames = backendController.getAllCompetitionNames();
+		assertTrue(!allCompetitionNames.isEmpty());
+		String[] compDetails = backendController.getCompetitionDetails(allCompetitionNames.get(0));
+		assertTrue(compDetails.length != 0);
+		assertTrue(backendController.getWinners(allCompetitionNames.get(0)) != null);
+	}
 	
+	@Test
+	public void testNumberAtGym() throws SQLException {
+		assertTrue(this.backendController.getNumberAtGym() >= 0);
+	}
+	
+	@Test
+	public void testGetStats() {
+		assertTrue(backendController.getUserStats(1).length == 4);
+		assertTrue(backendController.getGroupStats("male").length == 4);
+		assertTrue(backendController.getAllStats().length == 4);
+	}
+	
+	@Test
+	public void testGetGoal() throws NumberFormatException, SQLException {
+		assertTrue(Integer.valueOf(backendController.getGoal(1)) instanceof Integer);
+	}
 }
